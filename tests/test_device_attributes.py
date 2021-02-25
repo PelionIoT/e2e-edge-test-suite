0439d9bcae29489bf06b851d346bc2eb8b3fd0c3
Janne Hekkala <janne.hekkala@pelion.com>
2021-02-25 13:18:34 +0200

    Remove log level, remove unused test

--- tests/test_device_attributes.py
+++ tests/test_device_attributes.py
@@ -20,18 +20,11 @@ import logging
 import pytest
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
 
 
 @pytest.fixture(scope="module")
 def device_attributes(cloud_api, edge):
     yield cloud_api.device_directory.get_device(edge.device_id, expected_status_code=200).json()
-
-
-def test_list_attributes(cloud_api, edge, device_attributes):
-    log.info('Device: {} attributes:'.format(edge.device_id))
-    for key in device_attributes:
-        log.info('\t{} : {}'.format(key, device_attributes[key]))
 
 
 def test_device_registered(cloud_api, edge, device_attributes):
