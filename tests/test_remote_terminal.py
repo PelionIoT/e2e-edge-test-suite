02865658bec13aa337e7a5eb54398489c58a17bf
Janne Hekkala <janne.hekkala@pelion.com>
2021-03-03 12:41:58 +0200

    Simplify remote terminal

--- tests/test_remote_terminal.py
+++ tests/test_remote_terminal.py
@@ -36,18 +36,5 @@ def test_remote_terminal(edge):
     if not edge.has_remote_terminal:
         pytest.skip('Skipping because device don\'t have remote terminal supported.')
 
-    response = None
-    try:
-        response = edge.execute_remote_terminal('date +%A')
-    except BaseException as e:
-        log.info('Cannot execute using remote terminal: {}'.format(e))
-
-    weekdays = ['Sunday',
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday']
-
-    assert response in weekdays, 'Remote terminal connection not working correctly'
+    assert 'something' == edge.execute_remote_terminal('echo something'), 'Remote terminal connection not working ' \
+                                                                          'correctly'
